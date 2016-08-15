286e5cf663debf5c29f436c0f40c0071404437d5
susannasouv <ssouv@berkeley.edu>
2016-08-15 15:21:00 -0700

    GET and POST routes exist now for posts

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -5,8 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+
+mongoose.connect('mongodb://localhost/news');
 
 var app = express();
 
